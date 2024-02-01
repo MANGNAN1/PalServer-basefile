734a943a2b7b1a0c71c5dd6f752da2b113fc57ca
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-02 01:19:45 +0900

    Baseinstall.sh

--- Baseinstall.sh
+++ Baseinstall.sh
@@ -2,6 +2,12 @@
 # 사용자의 홈 디렉토리로 이동합니다.
 cd "$HOME" || exit
 
+# 사용자명을 동적으로 가져와 변수에 저장
+USERNAME=$(whoami)
+
+# 사용자의 홈 디렉토리 경로를 변수에 저장
+USER_HOME="/home/$USERNAME"
+
 #Start.sh Stop.sh Restart.sh Update.sh Manual.sh 설치 
 
 #echo -e "\e[32m베이스 파일들을 설치합니다.\e[0m"
@@ -25,7 +31,7 @@ curl -o Reserve.sh -O https://raw.githubusercontent.com/MANGNAN1/PalServer-basef
 echo -e "\e[32m완료.\e[0m"
 
 #실행권한 획득
-chmod +x ~/Start.sh ~/Stop.sh ~/Restart.sh ~/Update.sh ~/Manual.sh ~/Save.sh ~/Reserve.sh
+chmod +x $USER_HOME/Start.sh $USER_HOME/Stop.sh $USER_HOME/Restart.sh $USER_HOME/Update.sh $USER_HOME/Manual.sh $USER_HOME/Save.sh $USER_HOME/Reserve.sh
 
 #dos2unix 설치
 echo -e "\e[32mdos2uni를 설치합니다.\e[0m"
@@ -38,23 +44,23 @@ source ~/.bashrc
 
 #윈도우sh -> 리눅스sh 변환
 echo -e "\e[32msh 파일 변환을 실행합니다.\e[0m"
-dos2unix ~/Start.sh
-dos2unix ~/Stop.sh
-dos2unix ~/Restart.sh
-dos2unix ~/Update.sh
-dos2unix ~/Manual.sh
-dos2unix ~/Save.sh
-dos2unix ~/Reserve.sh
+dos2unix $USER_HOME/Start.sh
+dos2unix $USER_HOME/Stop.sh
+dos2unix $USER_HOME/Restart.sh
+dos2unix $USER_HOME/Update.sh
+dos2unix $USER_HOME/Manual.sh
+dos2unix $USER_HOME/Save.sh
+dos2unix $USER_HOME/Reserve.sh
 echo -e "\e[32m완료.\e[0m"
 
 #명령어 한글화
-alias 서버시작="~/Start.sh"
-alias 서버종료="~/Stop.sh"
-alias 서버리붓="~/Restart.sh"
-alias 업데이트="~/Update.sh"
-alias 사용법="~/Manual.sh"
-alias 저장="~/Save.sh"
-alias 예약="~/Reserve.sh"
+alias 서버시작="$USER_HOME/Start.sh"
+alias 서버종료="$USER_HOME/Stop.sh"
+alias 서버리붓="$USER_HOME/Restart.sh"
+alias 업데이트="$USER_HOME/Update.sh"
+alias 사용법="$USER_HOME/Manual.sh"
+alias 저장="$USER_HOME/Save.sh"
+alias 예약="$USER_HOME/Reserve.sh"
 
 # .bashrc 파일에서 명령어 갱신
 #echo -e "\e[32m명령어 한글화를 진행합니다.\e[0m"
@@ -67,13 +73,13 @@ update_bashrc() {
 
     # 추가할 내용
     append_text="# 명령어 한글화
-alias 서버시작=\"~/Start.sh\"
-alias 서버종료=\"~/Stop.sh\"
-alias 서버리붓=\"~/Restart.sh\"
-alias 업데이트=\"~/Update.sh\"
-alias 사용법=\"~/Manual.sh\"
-alias 저장=\"~/Save.sh\"
-alias 예약=\"~/Reserve.sh\"
+alias 서버시작=\"$USER_HOME/Start.sh\"
+alias 서버종료=\"$USER_HOME/Stop.sh\"
+alias 서버리붓=\"$USER_HOME/Restart.sh\"
+alias 업데이트=\"$USER_HOME/Update.sh\"
+alias 사용법=\"$USER_HOME/Manual.sh\"
+alias 저장=\"$USER_HOME/Save.sh\"
+alias 예약=\"$USER_HOME/Reserve.sh\"
 # 완료.
 "
 
