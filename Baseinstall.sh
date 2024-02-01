78942eda484d7f74f6038cc57ca80c129c5df4e9
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-01 23:23:30 +0900

    Baseinstall.sh

--- Baseinstall.sh
+++ Baseinstall.sh
@@ -10,6 +10,9 @@ wget https://raw.githubusercontent.com/MANGNAN1/PalServer-basefile/main/Update.s
 wget https://raw.githubusercontent.com/MANGNAN1/PalServer-basefile/main/Manual.sh
 echo -e "\e[32m완료.\e[0m"
 
+#실행권한 획득
+chmod +x Start.sh Stop.sh Restart.sh Update.sh Manual.sh
+
 #dos2unix 설치
 echo -e "\e[32mdos2uni를 설치합니다.\e[0m"
 sudo apt-get update
