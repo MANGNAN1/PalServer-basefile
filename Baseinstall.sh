1b502dcef66d766ee45fbd02106f83ec2c6db2ca
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-02 00:06:20 +0900

    Baseinstall.sh

--- Baseinstall.sh
+++ Baseinstall.sh
@@ -46,14 +46,44 @@ dos2unix ~/Save.sh
 echo -e "\e[32m완료.\e[0m"
 
 #명령어 한글화
-echo -e "\e[32m명령어 한글화를 진행합니다.\e[0m"
-alias 서버시작="~/Start.sh"
-alias 서버종료="~/Stop.sh"
-alias 서버리붓="~/Restart.sh"
-alias 업데이트="~/Update.sh"
-alias 사용법="~/Manual.sh"
-alias 저장="~/Save.sh"
-echo -e "\e[32m완료.\e[0m"
+#echo -e "\e[32m명령어 한글화를 진행합니다.\e[0m"
+#alias 서버시작="~/Start.sh"
+#alias 서버종료="~/Stop.sh"
+#alias 서버리붓="~/Restart.sh"
+#alias 업데이트="~/Update.sh"
+#alias 사용법="~/Manual.sh"
+#alias 저장="~/Save.sh"
+#echo -e "\e[32m완료.\e[0m"
+
+# .bashrc 파일에 명령어 추가
+append_to_bashrc() {
+    # .bashrc 파일 경로
+    bashrc_path="$HOME/.bashrc"
+
+    # 추가할 내용
+    append_text="# 명령어 한글화
+echo -e \"\\e[32m명령어 한글화를 진행합니다.\\e[0m\"
+alias 서버시작=\"~/Start.sh\"
+alias 서버종료=\"~/Stop.sh\"
+alias 서버리붓=\"~/Restart.sh\"
+alias 업데이트=\"~/Update.sh\"
+alias 사용법=\"~/Manual.sh\"
+alias 저장=\"~/Save.sh\"
+echo -e \"\\e[32m완료.\\e[0m\"
+"
+
+    # 이미 추가되어 있는지 확인
+    if grep -q "명령어 한글화" "$bashrc_path"; then
+        echo "이미 추가되어 있습니다."
+    else
+        # 파일 끝에 내용 추가
+        echo "$append_text" >> "$bashrc_path"
+        echo "추가되었습니다."
+    fi
+}
+
+# 함수 실행
+append_to_bashrc
 
 #새로고침
 source ~/.bashrc
