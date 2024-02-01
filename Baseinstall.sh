a3f831055fb69f2bf831b92bbabdf60a4913e3f2
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-01 22:59:15 +0900

    Baseinstall.sh

--- Baseinstall.sh
+++ Baseinstall.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 #Start.sh Stop.sh Restart.sh Update.sh 디렉토리 설치 
 
-echo "베이스 파일들을 설치합니다."
+echo -e "\e[32m베이스 파일들을 설치합니다.\e[0m"
 chmod +w ~
 wget https://raw.githubusercontent.com/MANGNAN1/PalServer-basefile/main/Start.sh
 wget https://raw.githubusercontent.com/MANGNAN1/PalServer-basefile/main/Restart.sh
 wget https://raw.githubusercontent.com/MANGNAN1/PalServer-basefile/main/Stop.sh
 wget https://raw.githubusercontent.com/MANGNAN1/PalServer-basefile/main/Update.sh
-echo "완료."
+echo -e "\e[32m완료.\e[0m"
 
 #dos2unix 설치
-echo "dos2uni를 설치합니다."
+echo -e "\e[32mdos2uni를 설치합니다.\e[0m"
 sudo apt-get update
 sudo apt-get install dos2unix
-echo "완료."
-
-#윈도우sh -> 리눅스sh 변환
-echo "sh 파일 변환을 실행합니다."
-dos2unix ~/Start.sh
-dos2unix ~/Stop.sh
-dos2unix ~/Restart.sh
-dos2unix ~/Update.sh
-echo "완료."
-
-#명령어 한글화
-echo "명령어 한글화를 진행합니다."
-alias 서버시작="~/Start.sh"
-alias 서버종료="~/Stop.sh"
-alias 서버리붓="~/Restart.sh"
-alias 업데이트="~/Update.sh"
-echo "완료."
+echo -e "\e[32m완료.\e[0m"
 
 #새로고침
 source ~/.bashrc
 
-echo "모든 작업을 완료하였습니다."
+#윈도우sh -> 리눅스sh 변환
+echo -e "\e[32msh 파일 변환을 실행합니다.\e[0m"
+dos2unix ~/Start.sh
+dos2unix ~/Stop.sh
+dos2unix ~/Restart.sh
+dos2unix ~/Update.sh
+echo -e "\e[32m완료.\e[0m"
+
+#명령어 한글화
+echo -e "\e[32m명령어 한글화를 진행합니다.\e[0m"
+alias 서버시작="~/Start.sh"
+alias 서버종료="~/Stop.sh"
+alias 서버리붓="~/Restart.sh"
+alias 업데이트="~/Update.sh"
+echo -e "\e[32m완료.\e[0m"
+
+#새로고침
+source ~/.bashrc
+
+echo -e "\e[32m모든 작업을 완료하였습니다.\e[0m"
