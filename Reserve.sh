772950023e2b178ce5a501837c15e02aec5b96a4
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-02 02:21:16 +0900

    Update Reserve.sh

--- Reserve.sh
+++ Reserve.sh
@@ -4,17 +4,24 @@
 read -p "작업을 추가하려면 '예약추가', 제거하려면 '예약제거'를 입력하세요: " action
 
 if [ "$action" == "예약추가" ]; then
+
+    # 사용자명을 동적으로 가져와 변수에 저장
+    USERNAME=$(whoami)
+    
+    # 사용자의 홈 디렉토리 경로를 변수에 저장
+    USER_HOME="/home/$USERNAME/Restart.sh"
+    
     # 사용자로부터 cron 표현식 입력 받기
     read -p "Cron 표현식을 입력하세요 (예: 0 */12 * * *): " cron_expression
 
-    # 스크립트 경로 입력 받기
-    read -p "실행할 스크립트의 경로를 입력하세요: " script_path
-
     # cron 표현식을 crontab에 추가
-    (crontab -l ; echo "$cron_expression $script_path") | crontab -
+    (crontab -l ; echo "$cron_expression $USER_HOME") | crontab -
 
     echo "작업이 추가되었습니다."
 elif [ "$action" == "예약제거" ]; then
+    #Crontab list
+    crontab -l
+
     # 사용자로부터 cron 표현식 입력 받기
     read -p "제거할 Cron 표현식을 입력하세요: " cron_expression
 
