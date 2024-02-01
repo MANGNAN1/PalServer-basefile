f72018bcc6ac91d8b80ad26f7819720d24cb869c
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-01 23:00:41 +0900

    Restart.sh

--- Restart.sh
+++ Restart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo -e "\e[32m서버를 리붓합니다.\e[0m"
 screen -S PalServerSession -X stuff $'\003'
 steamcmd +login anonymous +app_update 2394010 validate +quit
 screen -S PalServerSession -dm bash -c "cd ~/Steam/steamapps/common/PalServer && ./PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
+echo -e "\e[32m리붓 완료.\e[0m"
