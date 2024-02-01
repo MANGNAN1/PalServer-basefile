6547c7326866970d6a78443137ab903f2f76468d
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-02 01:34:12 +0900

    Baseinstall.sh

--- Baseinstall.sh
+++ Baseinstall.sh
@@ -54,45 +54,13 @@ dos2unix $USER_HOME/Reserve.sh
 echo -e "\e[32m완료.\e[0m"
 
 #명령어 한글화
-alias 서버시작="$USER_HOME/Start.sh"
-alias 서버종료="$USER_HOME/Stop.sh"
-alias 서버리붓="$USER_HOME/Restart.sh"
-alias 업데이트="$USER_HOME/Update.sh"
-alias 사용법="$USER_HOME/Manual.sh"
-alias 저장="$USER_HOME/Save.sh"
-alias 예약="$USER_HOME/Reserve.sh"
-
-# .bashrc 파일에서 명령어 갱신
-#echo -e "\e[32m명령어 한글화를 진행합니다.\e[0m"
-update_bashrc() {
-    # .bashrc 파일 경로
-    bashrc_path="$HOME/.bashrc"
-
-    # 기존 명령어 삭제
-    sed -i '/# 명령어 한글화/,/# 완료./d' "$bashrc_path"
-
-    # 추가할 내용
-    append_text="# 명령어 한글화
-alias 서버시작=\"$USER_HOME/Start.sh\"
-alias 서버종료=\"$USER_HOME/Stop.sh\"
-alias 서버리붓=\"$USER_HOME/Restart.sh\"
-alias 업데이트=\"$USER_HOME/Update.sh\"
-alias 사용법=\"$USER_HOME/Manual.sh\"
-alias 저장=\"$USER_HOME/Save.sh\"
-alias 예약=\"$USER_HOME/Reserve.sh\"
-# 완료.
-"
-
-    # 파일 끝에 내용 추가
-    echo "$append_text" >> "$bashrc_path"
-    echo -e "\e[32m갱신되었습니다.\e[0m"
-
-    # 변경사항 즉시 적용
-    source "$bashrc_path"
-}
-
-# 함수 실행
-update_bashrc
+#alias 서버시작="~/Start.sh"
+#alias 서버종료="~/Stop.sh"
+#alias 서버리붓="~/Restart.sh"
+#alias 업데이트="~/Update.sh"
+#alias 사용법="~/Manual.sh"
+#alias 저장="~/Save.sh"
+#alias 예약="~/Reserve.sh"
 
 #새로고침
 source ~/.bashrc
