be0a7f1ebcc7090525033b4765956d21f5fdbc8a
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-01 23:01:24 +0900

    Start.sh

--- Start.sh
+++ Start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo -e "\e[32m서버를 구동합니다.\e[0m"
 screen -S PalServerSession -dm bash -c "cd ~/Steam/steamapps/common/PalServer && ./PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
+echo -e "\e[32m구동완료.\e[0m"
