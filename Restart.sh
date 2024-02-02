df8c4353616bb9f2f2f938a0c1f50bcb11cc0901
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-02 22:21:44 +0900

    Update Restart.sh

--- Restart.sh
+++ Restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo -e "\e[32m서버를 리붓합니다.\e[0m"
+/home/b99qlrnrn/Save.sh
 screen -S PalServerSession -X stuff $'\003'
 steamcmd +login anonymous +app_update 2394010 validate +quit
 screen -S PalServerSession -dm bash -c "cd ~/Steam/steamapps/common/PalServer && ./PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
