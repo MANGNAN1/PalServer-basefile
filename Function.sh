b46a0721495dc0bcb48d322de23474b266d8298d
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-03 23:36:03 +0900

    Update Function.sh

--- Function.sh
+++ Function.sh
@@ -44,6 +44,15 @@ elif [ "$action" == "예약제거" ]; then
     #Crontab list
     crontab -r
 
+    # 사용자명을 동적으로 가져와 변수에 저장
+    USERNAME=$(whoami)
+    
+    # 사용자의 홈 디렉토리 경로를 변수에 저장
+    USER_HOME="/home/$USERNAME/Function.sh && Save"
+
+    # cron 표현식을 crontab에 추가
+    (crontab -l ; echo "0,30 * * * * $USER_HOME") | crontab -
+
     echo "작업이 제거되었습니다."
 
 elif [ "$action" == "리스트" ]; then
