08c48d91d43821c82bd6fa94804d0dee7825c426
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-04 19:54:04 +0900

    Update Function.sh

--- Function.sh
+++ Function.sh
@@ -143,6 +143,15 @@ fi
 # 서버리붓 함수
 Restart() {
     echo -e "\e[32m서버를 리붓합니다.\e[0m"
+    
+    # 사용자명을 동적으로 가져와 변수에 저장
+    USERNAME=$(whoami)
+    
+    # 사용자의 홈 디렉토리 경로를 변수에 저장
+    USER_HOME="/home/$USERNAME"
+    
+    $USER_HOME/Save.sh
+
     screen -S PalServerSession -X stuff $'\003'
     sleep 5
     steamcmd +login anonymous +app_update 2394010 validate +quit
