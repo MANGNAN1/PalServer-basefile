fa74a65fb72246fe3359e921a3e3e69dbad014ad
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-04 22:43:29 +0900

    Update Baseinstall.sh

--- Baseinstall.sh
+++ Baseinstall.sh
@@ -91,6 +91,7 @@ alias 최초설치='source $USER_HOME/Function.sh && Vminstall'
 alias 세팅='source $USER_HOME/Function.sh && Setting'
 alias 서버복구='source $USER_HOME/Function.sh && Restore'
 alias 공지='source $USER_HOME/Function.sh && Broadcast'
+alias admin='source $USER_HOME/Function.sh && Admin'
 alias test='source $USER_HOME/Function.sh && Test'
 # 완료.
 "
