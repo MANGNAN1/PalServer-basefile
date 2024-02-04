f2fe3c9c969bdcf695d0b2ef7f5a97ba03ff1aa1
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-04 22:36:40 +0900

    Update Function.sh

--- Function.sh
+++ Function.sh
@@ -375,10 +375,10 @@ fi
 }
 
 # 기능 테스트 함수
-Test() {
-    local user_message=$1  # 함수에 전달된 첫 번째 인자를 변수로 사용
-
+Broadcast() {
     USERNAME=$(whoami)
+
+    local user_message=$1  # 함수에 전달된 첫 번째 인자를 변수로 사용
 
     ini_file="/home/$USERNAME/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini"
     # Admin 패스워드 값 추출
@@ -391,6 +391,9 @@ Test() {
     #echo "AdminPassword: $ADMIN_PASSWORD"
     #echo "RCONPort: $RCON_PORT"
 
+    # 사용자에게 메시지 입력 받기
+    #read -r -p "할말: " user_message
+    
     # 사용자가 입력한 메시지를 'ARRCON'에 전달하여 실행
-    echo "$user_message" | ./ARRCON -P $RCON_PORT -p $ADMIN_PASSWORD
+    echo "broadcast $user_message" | ./ARRCON -P $RCON_PORT -p $ADMIN_PASSWORD   
 }
