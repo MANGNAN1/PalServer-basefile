7bfe6f5feba96c8d156416937b29d413c21d2f11
Sang9u <95752206+MANGNAN1@users.noreply.github.com>
2024-02-04 18:18:05 +0900

    Update Function.sh

--- Function.sh
+++ Function.sh
@@ -153,7 +153,7 @@ Restart() {
 # 파일 저장 함수
 Save() {
 SAVE_DIR="$HOME/backup/saved"
-DAYS_TO_KEEP=3
+MAX_BACKUPS=30
 
 if [ ! -d "$SAVE_DIR" ]; then
     mkdir -p "$SAVE_DIR" || { echo -e "\e[91m디렉터리 생성 실패: $SAVE_DIR\e[0m"; exit 1; }
@@ -176,8 +176,12 @@ if [ $? -eq 0 ]; then
     # 저장 폴더와 하위 폴더에 모든 권한 부여
     chmod -R 777 "$SAVE_DIR" || { echo -e "\e[91m권한 변경 실패: $SAVE_DIR\e[0m"; exit 1; }
 
-    # 5일 이상 된 파일 삭제
-    find "$SAVE_DIR" -type f -name "*.tar.gz" -mtime +$DAYS_TO_KEEP -exec rm {} \;
+    # 백업 데이터 개수가 MAX_BACKUPS를 넘으면 가장 오래된 데이터 삭제
+    while [ $(ls -1 "$SAVE_DIR" | grep -c '.tar.gz') -gt $MAX_BACKUPS ]; do
+        OLDEST_FILE=$(ls -1t "$SAVE_DIR" | grep '.tar.gz' | tail -n 1)
+        rm -f "$SAVE_DIR/$OLDEST_FILE"
+        echo -e "\e[33m가장 오래된 백업 데이터 삭제: $OLDEST_FILE\e[0m"
+    done
     
 else
     echo -e "\e[91m저장 중 오류가 발생했습니다. 로그를 확인하세요: $HOME/backup/error.log\e[0m"
